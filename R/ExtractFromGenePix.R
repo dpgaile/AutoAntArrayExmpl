a12f2fdf10962d09fe47f1c64dc5df7882d2f92e
Gaile <dpgaile@ad.buffalo.edu>
2017-01-04 09:23:15 -0500

    normalized raw slot added

--- R/ExtractFromGenePix.R
+++ R/ExtractFromGenePix.R
@@ -180,6 +180,13 @@ AAplotDens <- function(AAdat,SampleIndices,clrs=1,useMat="log",xlims=NA, returnM
         xlbl="Signal"
     }
 
+    if(useMat=="norm"){
+        W_1=AAdat$X_n_1
+        W_2=AAdat$X_n_2
+        xlbl="Normalized Signal"
+    }
+
+
     if(useMat=="raw.resid"){
         W_1=AAdat$X_resid_1
         W_2=AAdat$X_resid_2
